ab82ee5b397b9090f0bb6d4a4286922da3033782
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2024-09-09 11:41:18 -0300

    Create ConverteDados.java

---
+++ src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
@@ -0,0 +1,34 @@
+package br.com.alura.TabelaFipe.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.Collection;
+import java.util.List;
+
+// Classe para pegar os dados que estao sendo passado pela API e mandar para algum methods
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory()
+                            .constructCollectionType(List.class, classe);
+        try{
+            return mapper.readValue(json, lista);
+        }catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
